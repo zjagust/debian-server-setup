35d851890e70b05a75d55876e85779ac0ac25ae8
Zeljko Jagust <admin@bot-informatika.hr>
2020-04-06 14:24:44 +0200

    fixed whitespaces

--- debian-server-setup.sh
+++ debian-server-setup.sh
@@ -25,40 +25,40 @@ set -eio pipefail
 
 function initialize ()
 {
-	# Misc items
-	declare -gr SPACER='----------------------------------------------------------------------------------------------------'
-	declare -gr E=$'\e[1;31;103m'			# (E) Error: highlighted text.
-	declare -gr W=$'\e[1;31;103m'			# (W) Warning: highlighted text.
-	declare -gr B=$'\e[1m'				# B for Bold.
-	declare -gr R=$'\e[0m'				# R for Reset.
+    # Misc items
+    declare -gr SPACER='----------------------------------------------------------------------------------------------------'
+    declare -gr E=$'\e[1;31;103m'			# (E) Error: highlighted text.
+    declare -gr W=$'\e[1;31;103m'			# (W) Warning: highlighted text.
+    declare -gr B=$'\e[1m'				# B for Bold.
+    declare -gr R=$'\e[0m'				# R for Reset.
 
-	# Display a warning.
-	clear
+    # Display a warning.
+    clear
 
-	# Show a warning.
+    # Show a warning.
 	cat <<-END
  
-		${SPACER}
+        ${SPACER}
 
-		    ${B}** WARNING **${R}
+        ${B}** WARNING **${R}
 
-		    This script is only to be run on Debian 10 customized installations, as described on the following link:
-		    https://zacks.eu/debian-10-buster-initial-customization/
+            This script is only to be run on Debian 10 customized installations, as described on the following link:
+            https://zacks.eu/debian-10-buster-initial-customization/
 
-		    The script has a specific purpose and runs a certain tasks involving operating system environment
-		    customization and software installation. Running this script anywhere at any time will leave you
-		    with potentially un-bootable OS and software you may not want.
+            The script has a specific purpose and runs a certain tasks involving operating system environment
+            customization and software installation. Running this script anywhere at any time will leave you
+            with potentially un-bootable OS and software you may not want.
 
-		    A detailed overview of tasks this script will perform can be seen on the following link:
-		    https://zacks.eu/debian-10-buster-server-setup/
+            A detailed overview of tasks this script will perform can be seen on the following link:
+            https://zacks.eu/debian-10-buster-server-setup/
 
-		    Please make sure you understand what is written above!
+            Please make sure you understand what is written above!
 
-		${SPACER}
+        ${SPACER}
  
 	END
 
-	# Ask for confirmation.
+    # Ask for confirmation.
 	local ANSWER
 	read -rp "Type ${B}Y${R} to proceed, or anything else to cancel, and press Enter: ${B}" ANSWER
 	echo "${R}"
